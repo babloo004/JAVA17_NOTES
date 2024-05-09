c6cd42c42d275cb0fdbcc6a871cc267d6c124353
Avinash <129788500+babloo004@users.noreply.github.com>
2024-05-09 14:56:04 +0530

    Add files via upload

---
+++ 09-accessspecifiers.java
@@ -0,0 +1,15 @@
+//Access specifiers
+//In Java, access specifiers are keywords used to control the visibility and accessibility of classes, methods, and variables. 
+//to implement encapsulation  we use the "private" access specifier
+//there are four specifier 1.public 2.private 3.protected 4.default
+//___________________________________________________________________________________________________
+//|   Access Specifier	|Within Class	|  Same Package	  |  Subclass	|   Different Package       |
+//|_____________________|_______________|_________________|_____________|__________________________ |
+//| public	            | Yes	        |      Yes	      |      Yes    |	    Yes                 |
+//|_____________________|_______________|_________________|_____________|__________________________ |
+//|  protected	        |  Yes	        |      Yes        | 	 Yes    |    	No                  |
+//|_____________________|_______________|_________________|_____________|__________________________ |
+//|  default	        |  Yes	        |      Yes	      |      No	    |       No                  |
+//|_____________________|_______________|_________________|_____________|__________________________ |
+//|  private  	        |  Yes	        |      No	      |      No	    |       No                  |
+//|_____________________|_______________|_________________|_____________|__________________________ |
