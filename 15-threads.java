f7135b4ee0d8a44f84cf4adc729b3df660f63e3e
Avinash <129788500+babloo004@users.noreply.github.com>
2024-05-11 12:01:46 +0530

    Update 15-threads.java

--- 15-threads.java
+++ 15-threads.java
@@ -1,3 +1,11 @@
+//multitasking
+//in java multitasking can be achieved by two ways 1. Process-Based Multitasking 2. Thread-Based Multitasking
+//In process based multitasking, proccess are heavy weight and communication between each process is very costly
+//in process based multitasking, each process takes different memory loacation
+//where as threads are light weight,and the cost of communication is also very low compared to the process based multitasking
+//so we opt for threads based multi tasking
+
+
 //threads
 //Threads in java are used for multi tasking
 //Threads share the same memory
@@ -6,6 +14,30 @@
 //When you run the code with two threads (A and B) concurrently, the operating system assigns time slices to each thread on the available CPU cores. 
 //These time slices are small intervals during which a thread is allowed to execute its instructions. The operating system rapidly switches between threads, allowing each one to make progress.
 //in single core CPUs threads wont run simultaneously,they appear to run at same time
+
+//states of thread
+
+//new state - the code is just defined in the run method but not executed
+
+//active state - the thread will convert into active state, by intiating the start() method
+//active state consists two staes, they are runnable state and running state
+//in runnable state the thread is ready to execute at any given time, its the job of thread scheduler to allot time for thread to run
+//running state the thread will get cpu to get execute and the thread will execute, but it will again goes to the runnable state after expiry of given time slice
+
+//. Waiting/Blocked State 
+//If a Thread is inactive but on a temporary time, then either it is a waiting or blocked state.
+//For example, if there are two threads, T1 and T2 where T1 needs to communicate to the camera and the other thread T2 already using a camera to scan then T1 waits until T2 Thread completes its work, at this state T1 is parked in waiting for the state
+
+//Timed Waiting State
+//Sometimes the longer duration of waiting for threads causes starvation
+// if we take an example like there are two threads T1, T2 waiting for CPU and T1 is undergoing a Critical Coding operation and if it does not exist the CPU until its operation gets executed then T2 will be exposed to longer waiting with undetermined certainty, In order to avoid this starvation situation, we had Timed Waiting for the state to avoid that kind of scenario as in Timed Waiting, each thread has a time period for which sleep() method is invoked and after the time expires the Threads starts executing its task. 
+//. When there are multiple threads parked in a Blocked/Waiting state Thread Scheduler clears Queue by rejecting unwanted Threads and allocating CPU on a priority basis. 
+
+//Terminated State
+//A thread will be in Terminated State, due to the below reasons: 
+// Termination is achieved by a Thread when it finishes its task Normally.
+// Sometimes Threads may be terminated due to unusual events like segmentation faults, exceptions…etc. and such kind of Termination can be called Abnormal Termination.
+// A terminated Thread means it is dead and no longer available.
 
 //We can create threads in java by two methods 1. by extending the Thread class 2. by implementing Runnable interface
 //Runnable interface contains run() method, in that method we should write our code
